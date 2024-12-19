46787286a04b538b0e3a4fc31bca496024b6ec8c
Hyeonjoon Kim <hyeon00n@naver.com>
2024-12-19 16:49:56 +0900

    필수 6. Dynamic Insert

--- src/main/java/com/example/demo/entity/Item.java
+++ src/main/java/com/example/demo/entity/Item.java
@@ -2,8 +2,10 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.DynamicInsert;
 
 
+@DynamicInsert
 @Entity
 @Getter
 // TODO: 6. Dynamic Insert
@@ -24,7 +26,7 @@ public class Item {
     @JoinColumn(name = "manager_id")
     private User manager;
 
-    @Column(nullable = false, columnDefinition = "varchar(20) default 'PENDING'")
+    @Column(columnDefinition = "varchar(20) default 'PENDING'")
     private String status;
 
     public Item(String name, String description, User manager, User owner) {
