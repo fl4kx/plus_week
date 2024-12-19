cf9caf029414f3ea85268424f9d6365e6e49ea72
Hyeonjoon Kim <hyeon00n@naver.com>
2024-12-19 13:25:59 +0900

    fix: 영속성 컨텍스트 어노테이션 추가

--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.entity.Status;
 import com.example.demo.repository.UserRepository;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.apache.commons.collections4.ListUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +13,8 @@ import java.util.List;
 @Service
 public class AdminService {
     private final UserRepository userRepository;
+
+    @PersistenceContext
     private EntityManager em;
 
     public AdminService(UserRepository userRepository) {
